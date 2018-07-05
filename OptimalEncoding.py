74960b7592f48ef7f85200cc83916d06978a551b
Hudson Cooper <hncooper@ucsd.edu>
2018-07-05 13:47:48 -0700

    Heteroskedastic

--- OptimalEncoding.py
+++ OptimalEncoding.py
@@ -160,3 +160,156 @@ class OptimalEncoding(object):
     
     def decode(self, z):
         return(self.sess.run(self.decoder(z, linear_out=True))[:,:-1])
+    
+
+class OptimalEncoding_Weight_Noise(object):
+    def __init__(self, encoder, decoder, k):
+        #Encoder n_out must equal k
+        self.encoder = encoder
+        #Decoder n_in must equal k
+        self.decoder = decoder
+        
+        self.params = encoder.params + decoder.params
+        
+        self.X_dim = encoder.n_in
+        self.Y_dim = decoder.n_out - 1 # extra dimension for model (spherical) variance
+        self.k = k
+        
+        #Data
+        self.X = tf.placeholder(tf.float32, shape=[None, self.X_dim], name='X')
+        self.Y = tf.placeholder(tf.float32, shape=[None, self.Y_dim], name='Y')
+        
+        
+        #Noise
+        #placeholder for epsilon
+        self.epsilon = [
+            tf.placeholder(tf.float32, shape=(None, width), name = 'epsilon')
+            for width in encoder.widths
+        ]
+        
+        #Encode
+        self.Z = encoder(self.X, self.epsilon, linear_out=True)
+        
+        #Decode
+        self.Y_hat = decoder(self.Z, linear_out=True)
+        self.std = self.Y_hat[:,-1]
+        self.Y_hat = self.Y_hat[:,:-1]
+        
+        #Losses 
+        #Autoencoder 
+        self.AD = tf.reduce_sum(
+                tf.abs(self.Y - tf.tanh(self.Y_hat)),
+                axis=1
+            )
+        self.Laplace_Homoskedastic = tf.log(tf.reduce_mean(self.AD))
+        self.Laplace_Heteroskedastic = tf.reduce_mean(tf.log(self.AD))
+        
+        #Classification
+        self.CrossEnt = tf.reduce_mean(
+            tf.nn.softmax_cross_entropy_with_logits_v2(labels=tf.stop_gradient(self.Y), logits=self.Y_hat)
+        )
+        
+        
+    def train(self, x, y=None, min_entropy=True, epochs=100, batch_size=64, lr=1e-3, sigma = 1, bandwidth=-1, task = 'autoencoder', heteroskedastic = True):
+        
+        self.Laplace = self.Laplace_Heteroskedastic if heteroskedastic else self.Laplace_Homoskedastic
+        
+        taskdict = {
+            'autoencoder': self.Laplace,
+            'classification': self.CrossEnt
+        }
+        
+        y = (x if y is None else y)
+        
+        
+        #Stein entropy
+        self.Entropy =  tf.reduce_mean(tf.einsum('ij,ij->i', self.Z, tf.stop_gradient(lib.stein_d_H(self.Z, bandwidth))))
+        
+        if task in taskdict:
+            self.taskLoss = taskdict[task]
+            self.Loss =  (self.Entropy if min_entropy else tf.stop_gradient(self.Entropy)) + self.taskLoss 
+        else:
+            raise ValueError('task not supported yet')
+
+        #Optimizer 
+        solver = tf.train.RMSPropOptimizer(learning_rate = lr).minimize(self.Loss, var_list=self.params)
+        
+        
+        #Training
+        init = tf.global_variables_initializer()
+        sess = tf.Session()
+        self.sess = sess
+        with sess.as_default():
+            sess.run(init)
+
+            losses = []
+            tasklosses = []
+            ents = []
+            knn_ents = []
+            
+            n_batches = int(x.shape[0]/float(batch_size))
+            for epoch in tqdm(range(epochs)):        
+                rand_idxs = np.arange(x.shape[0]) 
+                np.random.shuffle(rand_idxs)
+                
+                loss = 0
+                task_loss = 0
+                ent = 0
+                
+                for batch in range(n_batches):
+                    mb_idx = rand_idxs[batch*batch_size:(batch+1)*batch_size]
+                    x_mb = x[mb_idx]
+                    y_mb = y[mb_idx]
+                    
+                    epsilon = [
+                        np.random.normal(0,1, size=(batch_size, eps.shape[-1]))
+                        for eps in self.epsilon
+                    ]
+                    
+                    _, loss_curr, taskloss_curr, ent_curr = sess.run(
+                        [
+                            solver, self.Loss, self.taskLoss, self.Entropy
+                        ], 
+                        feed_dict = {self.X:x_mb, self.Y:y_mb, self.epsilon:epsilon, self.sigma:sigma})
+                    
+                    loss += loss_curr/n_batches
+                    task_loss += taskloss_curr/n_batches
+                    ent += ent_curr/n_batches
+                    
+                zhat = self.encode(x_mb, sigma=sigma)
+                knn_ent = entropy(zhat)
+                
+                losses.append(loss)
+                tasklosses.append(task_loss)
+                ents.append(ent)
+                knn_ents.append(knn_ent)
+            
+            print('Sigma: %f' %(sigma))
+
+            print('Final task loss: %f' %(tasklosses[-1]))
+            
+            plt.figure()
+            plt.plot(losses)
+            plt.title('total loss')
+            
+            plt.figure()
+            plt.plot(tasklosses)
+            plt.title('task loss')
+            
+            plt.figure()
+            plt.plot(np.array(ents))
+            plt.title('pseudo entropy loss')
+            
+            plt.figure()
+            plt.title('knn estimated entropy')
+            plt.plot(knn_ents)
+
+    def encode(self, x, sigma = 0):
+        epsilon = [
+                        np.random.normal(0,1, size=(batch_size, eps.shape[-1]))
+                        for eps in self.epsilon
+                    ]
+        return(self.sess.run(self.Z, feed_dict = {self.X:x, self.epsilon:epsilon, self.sigma:sigma}))
+    
+    def decode(self, z):
+        return(self.sess.run(self.decoder(z, linear_out=True))[:,:-1])
--- networks.py
+++ networks.py
@@ -49,4 +49,52 @@ class MLP(object):
         return(value)
             
             
+class noisy_MLP(object):
+    def __init__(self, n_in, n_out, depth, widths, activation = tf.tanh, 
+                 random_seed = None):
+        
+        self.n_in = n_in
+        self.n_out = n_out
+        self.depth = depth
+        self.widths = widths
+        self.activation = activation
+        self.random_seed = random_seed
+        
+        xavier_init = xavier_initializer(seed = random_seed)
+        layers = {}
+        params = []
+        assert depth == len(widths), 'Depth %d != len(widths) %d' %(depth, len(widths))
+        for i in range(depth+1):
+            layer = {
+                'W' :  tf.Variable(xavier_init(
+                    [
+                        n_in if i == 0 else widths[i-1], 
+                        n_out if i == depth else widths[i]
+                    ]), 
+                                       name = 'W%d'%i),
+                'b' : tf.Variable(tf.zeros(
+                    [
+                        n_out if i == depth else widths[i]
+                    ]), 
+                                      name = 'b%d'%i)
+            }
+            params = params + list(layer.values())
+            layers.update({i : layer})
+            
+        self.layers = layers 
+        self.params = params
+         
+    def __call__(self, x, epsilon, linear_out = True):
+        assert x.shape[1] == self.n_in, 'x.shape[1] %d != self.n_in %d' %(x.shape[1], self.n_in)
+        value = x
+        for i in range(self.depth + 1):
+            W = self.layers[i]['W']
+            b = self.layers[i]['b']
+            if i == self.depth and linear_out:
+                value = value @ W * epsilon[i-1] + b
+            else:
+                value = self.activation(value @ W * (epsilon[i-1] if i > 1 else 1) + b)
+        return(value)
+            
+            
             
