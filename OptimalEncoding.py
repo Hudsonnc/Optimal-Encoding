702faf2de15f11c728f8d60ee31e10185c5af6cb
Hudson Cooper <hncooper@ucsd.edu>
2018-07-15 14:23:11 -0700

    multi-sample variance estimation

--- OptimalEncoding.py
+++ OptimalEncoding.py
@@ -24,7 +24,7 @@ class OptimalEncoding(object):
         self.params = encoder.params + decoder.params
         
         self.X_dim = encoder.n_in
-        self.Y_dim = decoder.n_out - 1 # extra dimension for model (spherical) variance
+        self.Y_dim = decoder.n_out
         self.k = k
         
         #Data
@@ -33,41 +33,78 @@ class OptimalEncoding(object):
         
         #Encode
         self.f_X = encoder(self.X, linear_out=True)
+        #placeholder for n_samples in latent space
+        self.n_samples = tf.placeholder(tf.int32, shape = (), name = 'n_samples')
         #placeholder for epsilon
-        self.epsilon = tf.placeholder(tf.float32, shape=(None, self.k), name = 'epsilon')
+        self.epsilon = tf.placeholder(tf.float32, shape=(None, None, self.k), name = 'epsilon')
         #placeholder for sigma
         self.sigma = tf.placeholder(tf.float32, shape=(), name = 'sigma')
         #Add noise to encoding
-        self.Z = self.f_X + (self.sigma * self.epsilon)
+        self.Z = tf.expand_dims(self.f_X, 1) + (self.sigma * self.epsilon)
         
-        #Decode
-        self.Y_hat = decoder(self.Z, linear_out=True)
-        self.std = self.Y_hat[:,-1]
-        self.Y_hat = self.Y_hat[:,:-1]
+        #Decode: reshape Z to (None*n_samples, k) to decode and then reshape back to (None, n_samples, y_dim) 
+        self.Y_hat = tf.reshape(
+            decoder(
+                tf.reshape(
+                    self.Z,
+                    tf.stack([-1, self.k])
+                ),
+                linear_out=True
+            ),
+            tf.stack([-1, self.n_samples, self.Y_dim])
+        )
         
         #Losses 
         #Autoencoder 
-        self.AD = tf.reduce_sum(
-                tf.abs(self.Y - activation(self.Y_hat)),
-                axis=1
+        self.Eltwise_Deviation = tf.abs(tf.expand_dims(self.Y, 1) - activation(self.Y_hat))
+        self.Laplace_Homoskedastic = tf.reduce_sum(#Sum result over features to get scalar (diagonal covariance)
+            tf.log(#log of mean features across all batchsize * n_samples (homoskedastic in columns)
+                tf.reduce_mean(#Mean across batchsize, n_samples.
+                    self.Eltwise_Deviation, 
+                    axis=(0,1)
+                ) + EPS
             )
-        self.Laplace_Homoskedastic = tf.log(tf.reduce_mean(self.AD) + EPS)
-        self.Laplace_Heteroskedastic = tf.reduce_mean(tf.log(self.AD + EPS))
-        #self.Laplace = tf.reduce_mean(self.AD)
+        )
+        self.Laplace_Heteroskedastic = tf.reduce_mean(#Mean over batch_size to get scalar
+            tf.reduce_sum(#sum over features (diagonal covariance)
+                tf.log(#log of mean features across n_samples
+                    tf.reduce_mean(#mean across n_samples
+                        self.Eltwise_Deviation,
+                        axis=(1)
+                    ) + EPS
+                ),
+                axis=(-1)
+            )
+        )
         
         #Classification
-        #self.CrossEnt = tf.reduce_mean(
-         #   tf.nn.softmax_cross_entropy_with_logits(labels=tf.stop_gradient(self.Y), logits=self.Y_hat)
-       # )
-        self.CrossEnt_Expanded = tf.einsum(
-           'ij,ij->i',
+        self.Samplewise_CrossEnt = tf.einsum(
+           'ik,ijk->ij',
            self.Y, 
-            -tf.log(tf.exp(self.Y_hat) / (tf.expand_dims(tf.reduce_sum(tf.exp(self.Y_hat), 1), axis=-1) + EPS) + EPS)
+            -tf.log(tf.exp(self.Y_hat) / (tf.expand_dims(tf.reduce_sum(tf.exp(self.Y_hat), axis=-1), axis=-1) + EPS) + EPS)
         )
-        self.CrossEnt_Homoskedastic = .5*tf.log(tf.reduce_mean(self.CrossEnt_Expanded) + EPS)
-        self.CrossEnt_Heteroskedastic = .5*tf.reduce_mean(tf.log(self.CrossEnt_Expanded + EPS))
+        self.CrossEnt_Homoskedastic = .5*tf.log(
+            tf.reduce_mean(#Mean over batchsize, n_samples 
+                self.Samplewise_CrossEnt
+            ) + EPS
+        )
+        self.CrossEnt_Heteroskedastic = .5*tf.reduce_mean( #Mean over batchsize
+            tf.log(
+                tf.reduce_mean(#Mean over n_samples
+                    self.Samplewise_CrossEnt,
+                    axis = (1)
+                ) + EPS
+            )
+        )
         
-    def train(self, x, y=None, min_entropy=True, epochs=100, batch_size=64, lr=1e-3, sigma = 1, bandwidth=-1, task = 'autoencoder', heteroskedastic = False):
+        #Stein entropy
+        flat_Z = tf.reshape(
+                    self.Z,
+                    tf.stack([-1, self.k])
+                )
+        self.Entropy =  tf.reduce_mean(tf.einsum('ij,ij->i', flat_Z, tf.stop_gradient(lib.stein_d_H(flat_Z, -1))))
+        
+    def train(self, x, y=None, min_entropy=True, epochs=100, batch_size=64, lr=1e-3, n_samples = 1, sigma = 1, task = 'autoencoder', heteroskedastic = False):
         
         self.Laplace = self.Laplace_Heteroskedastic if heteroskedastic else self.Laplace_Homoskedastic
         self.CrossEnt = self.CrossEnt_Heteroskedastic if heteroskedastic else self.CrossEnt_Homoskedastic
@@ -79,10 +116,6 @@ class OptimalEncoding(object):
         
         y = (x if y is None else y)
         
-        
-        #Stein entropy
-        self.Entropy =  tf.reduce_mean(tf.einsum('ij,ij->i', self.Z, tf.stop_gradient(lib.stein_d_H(self.Z, bandwidth))))
-        
         if task in taskdict:
             self.taskLoss = taskdict[task]
             self.Loss =  (self.Entropy if min_entropy else tf.stop_gradient(self.Entropy)) + self.taskLoss 
@@ -91,7 +124,6 @@ class OptimalEncoding(object):
 
         #Optimizer 
         solver = tf.train.RMSPropOptimizer(learning_rate = lr).minimize(self.Loss, var_list=self.params)
-        
         
         #Training
         init = tf.global_variables_initializer()
@@ -114,24 +146,18 @@ class OptimalEncoding(object):
                 task_loss = 0
                 ent = 0
                 
-                #sigma = sess.run(
-                #        lib.h(self.f_X), 
-                #        feed_dict = {self.X:x}
-                #    )
-                #sigma = np.sqrt(sigma)
-                
                 for batch in range(n_batches):
                     mb_idx = rand_idxs[batch*batch_size:(batch+1)*batch_size]
                     x_mb = x[mb_idx]
                     y_mb = y[mb_idx]
                     
-                    epsilon = np.random.normal(0,1, size=(batch_size, self.k))
+                    epsilon = np.random.normal(0,1, size=(batch_size, n_samples, self.k))
                     
                     _, loss_curr, taskloss_curr, ent_curr = sess.run(
                         [
                             solver, self.Loss, self.taskLoss, self.Entropy
                         ], 
-                        feed_dict = {self.X:x_mb, self.Y:y_mb, self.epsilon:epsilon, self.sigma:sigma})
+                        feed_dict = {self.X:x_mb, self.Y:y_mb, self.epsilon:epsilon, self.sigma:sigma, self.n_samples: n_samples})
                     
                     loss += loss_curr/n_batches
                     task_loss += taskloss_curr/n_batches
@@ -166,161 +192,10 @@ class OptimalEncoding(object):
             plt.plot(knn_ents)
 
     def encode(self, x, sigma = 0):
-        epsilon = np.random.normal(0,1, size=(len(x), self.k))
-        return(self.sess.run(self.Z, feed_dict = {self.X:x, self.epsilon:epsilon, self.sigma:sigma}))
+        epsilon = np.random.normal(0,1, size=(len(x), 1, self.k))
+        return(self.sess.run(self.Z, feed_dict = {self.X:x, self.epsilon:epsilon, self.sigma:sigma, self.n_samples: 1})[:,0,:])
     
     def decode(self, z):
-        return(self.sess.run(self.decoder(z, linear_out=True))[:,:-1])
+        return(self.sess.run(self.decoder(z, linear_out=True)))
     
 
-class OptimalEncoding_Weight_Noise(object):
-    def __init__(self, encoder, decoder, k):
-        #Encoder n_out must equal k
-        self.encoder = encoder
-        #Decoder n_in must equal k
-        self.decoder = decoder
-        
-        self.params = encoder.params + decoder.params
-        
-        self.X_dim = encoder.n_in
-        self.Y_dim = decoder.n_out - 1 # extra dimension for model (spherical) variance
-        self.k = k
-        
-        #Data
-        self.X = tf.placeholder(tf.float32, shape=[None, self.X_dim], name='X')
-        self.Y = tf.placeholder(tf.float32, shape=[None, self.Y_dim], name='Y')
-        
-        
-        #Noise
-        #placeholder for epsilon
-        self.epsilon = [
-            tf.placeholder(tf.float32, shape=(None, width), name = 'epsilon')
-            for width in encoder.widths
-        ]
-        
-        #Encode
-        self.Z = encoder(self.X, self.epsilon, linear_out=True)
-        
-        #Decode
-        self.Y_hat = decoder(self.Z, linear_out=True)
-        self.std = self.Y_hat[:,-1]
-        self.Y_hat = self.Y_hat[:,:-1]
-        
-        #Losses 
-        #Autoencoder 
-        self.AD = tf.reduce_sum(
-                tf.abs(self.Y - tf.tanh(self.Y_hat)),
-                axis=1
-            )
-        self.Laplace_Homoskedastic = tf.log(tf.reduce_mean(self.AD))
-        self.Laplace_Heteroskedastic = tf.reduce_mean(tf.log(self.AD))
-        
-        #Classification
-        self.CrossEnt = tf.reduce_mean(
-            tf.nn.softmax_cross_entropy_with_logits_v2(labels=tf.stop_gradient(self.Y), logits=self.Y_hat)
-        )
-        
-        
-    def train(self, x, y=None, min_entropy=True, epochs=100, batch_size=64, lr=1e-3, sigma = 1, bandwidth=-1, task = 'autoencoder', heteroskedastic = True):
-        
-        self.Laplace = self.Laplace_Heteroskedastic if heteroskedastic else self.Laplace_Homoskedastic
-        
-        taskdict = {
-            'autoencoder': self.Laplace,
-            'classification': self.CrossEnt
-        }
-        
-        y = (x if y is None else y)
-        
-        
-        #Stein entropy
-        self.Entropy =  tf.reduce_mean(tf.einsum('ij,ij->i', self.Z, tf.stop_gradient(lib.stein_d_H(self.Z, bandwidth))))
-        
-        if task in taskdict:
-            self.taskLoss = taskdict[task]
-            self.Loss =  (self.Entropy if min_entropy else tf.stop_gradient(self.Entropy)) + self.taskLoss 
-        else:
-            raise ValueError('task not supported yet')
-
-        #Optimizer 
-        solver = tf.train.RMSPropOptimizer(learning_rate = lr).minimize(self.Loss, var_list=self.params)
-        
-        
-        #Training
-        init = tf.global_variables_initializer()
-        sess = tf.Session()
-        self.sess = sess
-        with sess.as_default():
-            sess.run(init)
-
-            losses = []
-            tasklosses = []
-            ents = []
-            knn_ents = []
-            
-            n_batches = int(x.shape[0]/float(batch_size))
-            for epoch in tqdm(range(epochs)):        
-                rand_idxs = np.arange(x.shape[0]) 
-                np.random.shuffle(rand_idxs)
-                
-                loss = 0
-                task_loss = 0
-                ent = 0
-                
-                for batch in range(n_batches):
-                    mb_idx = rand_idxs[batch*batch_size:(batch+1)*batch_size]
-                    x_mb = x[mb_idx]
-                    y_mb = y[mb_idx]
-                    
-                    epsilon = [
-                        np.random.normal(0,1, size=(batch_size, eps.shape[-1]))
-                        for eps in self.epsilon
-                    ]
-                    
-                    _, loss_curr, taskloss_curr, ent_curr = sess.run(
-                        [
-                            solver, self.Loss, self.taskLoss, self.Entropy
-                        ], 
-                        feed_dict = {self.X:x_mb, self.Y:y_mb, self.epsilon:epsilon, self.sigma:sigma})
-                    
-                    loss += loss_curr/n_batches
-                    task_loss += taskloss_curr/n_batches
-                    ent += ent_curr/n_batches
-                    
-                zhat = self.encode(x_mb, sigma=sigma)
-                knn_ent = entropy(zhat)
-                
-                losses.append(loss)
-                tasklosses.append(task_loss)
-                ents.append(ent)
-                knn_ents.append(knn_ent)
-            
-            print('Sigma: %f' %(sigma))
-
-            print('Final task loss: %f' %(tasklosses[-1]))
-            
-            plt.figure()
-            plt.plot(losses)
-            plt.title('total loss')
-            
-            plt.figure()
-            plt.plot(tasklosses)
-            plt.title('task loss')
-            
-            plt.figure()
-            plt.plot(np.array(ents))
-            plt.title('pseudo entropy loss')
-            
-            plt.figure()
-            plt.title('knn estimated entropy')
-            plt.plot(knn_ents)
-
-    def encode(self, x, sigma = 0):
-        epsilon = [
-                        np.random.normal(0,1, size=(batch_size, eps.shape[-1]))
-                        for eps in self.epsilon
-                    ]
-        return(self.sess.run(self.Z, feed_dict = {self.X:x, self.epsilon:epsilon, self.sigma:sigma}))
-    
-    def decode(self, z):
-        return(self.sess.run(self.decoder(z, linear_out=True))[:,:-1])
